676b846b77dedf69877ff4fb2e3a86b18e77b169
marcus1060 <git@marcusds.ca>
2012-10-21 20:51:13 -0700

    Lots of improvements
    Remove elements after loaded. Load as IMGs not IFRAMES. If image is
    taking to long, remove it even if not done. More to come still.

--- preload_gifs.js
+++ preload_gifs.js
@@ -1,19 +1,32 @@
-function preload_gif( url ) {
-	var frame = document.createElement( 'iframe' );
-	frame.src = url;
-	frame.style.display = 'none';
-	document.body.appendChild( frame );
+var devmode = true;
+function preload_gif(url) {
+	var img = document.createElement('img');
+	img.src = url;
+	if(devmode == false) {
+		img.style.display = 'none';
+	}
+	document.body.appendChild(img);
+	img.onload = function() {
+		clearTimeout(timeout);
+		if(devmode == false) {
+			img.parentNode.removeChild(img);
+		}
+	};
+	var timeout = setTimeout(function() { if(img) { img.parentNode.removeChild(img); } }, 30000);
+}
+
+function frame_done(id, frame) {
+	frame.parentNode.removeChild(frame);
 }
 
 function find_gifs() {
-	var links = document.getElementsByTagName( 'a' );
+	var links = document.getElementsByClassName('title');
 	var i = links.length;
-	
 	while ( i-- ) {
-		if ( links[i].getAttribute( 'href' ) != null && links[i].getAttribute( 'href' ).slice( -3 ) == 'gif' ) {
-			preload_gif( links[i].getAttribute( 'href' ) );
+		if ( links[i].tagName == 'A' && links[i].getAttribute('href') != null && links[i].getAttribute('href').slice(-4) == '.gif' ) {
+			preload_gif(links[i].getAttribute('href'));
 		}
 	}
 }
 
-window.addEventListener( "load", find_gifs() );
+window.addEventListener('load', function() { setTimeout(1000, find_gifs()); });
