b493cd49b8f7d1c3e404b6e6f3abb9d0906654fb
marcus1060 <git@marcusds.ca>
2012-10-29 15:40:43 -0700

    Option to only download non-nsfw

--- preload_gifs.js
+++ preload_gifs.js
@@ -1,4 +1,5 @@
 var devmode = true;
+var ns = 1; // 0 = all, 1 = only non-nsfw, 2 = only nsfw
 function preload_gif(url) {
 	var img = document.createElement('img');
 	img.src = url;
@@ -26,9 +27,31 @@ that.parentNode.removeChild(that);
 function find_gifs() {
 	var links = document.getElementsByClassName('title');
 	var i = links.length;
+	var href;
 	while ( i-- ) {
-		if ( links[i].tagName == 'A' && links[i].getAttribute('href') != null && links[i].getAttribute('href').slice(-4) == '.gif') {
-			preload_gif(links[i].getAttribute('href'));
+		href = links[i].getAttribute('href');
+		if(href == null) {
+			continue;
+		}
+		if ( links[i].tagName == 'A' && href != null && (
+			href.slice(-4) == '.gif'
+			|| href.search('.gif?') > 0)
+			/*|| (document.URL.search('gif') > -1 && href.search('imgur.com') >= 0 && href.search('imgur.com/a/') == -1) // if on a gif reddit try and preload imgur GIFs linked to page instead of GIF*/
+		) {
+			/*if(href.search('imgur.com') >= 0 && href.search('imgur.com/a/') == -1) {
+				href = href + '.gif';
+			}*/
+			if(ns == 1) {
+				if(links[i].parentNode.parentNode.childNodes[2].getElementsByClassName('nsfw-stamp').length == 0) {
+					preload_gif(href);
+				}
+			} else if(ns == 2) {
+				if(links[i].parentNode.parentNode.childNodes[2].getElementsByClassName('nsfw-stamp').length > 0) {
+					preload_gif(href);
+				}
+			} else {
+				preload_gif(href);
+			}
 		}
 	}
 }
