07dd15d78ce24dd443df631d3431aae5c3a8a519
marcusds <git@marcusds.ca>
2013-04-08 23:44:46 -0700

    V1.3 - Support for never ending reddit from RES. Also gifs preload top-down now.

--- preload_gifs.js
+++ preload_gifs.js
@@ -1,6 +1,7 @@
 var devmode = true;
-var ns = 1; // 0 = all, 1 = only non-nsfw, 2 = only nsfw
-function preload_gif(url) {
+var ns = 0; // 0 = all, 1 = only non-nsfw, 2 = only nsfw
+var NERCount = 0;
+function preload_gif(url, link) {
 	var img = document.createElement('img');
 	img.src = url;
 	img.style.display = 'none';
@@ -10,8 +11,12 @@ function preload_gif(url) {
 		if(devmode == false) {
 			img.parentNode.removeChild(img);
 		} else {
-			img.style.display = 'inline';
+			//img.style.display = 'inline';
+			img.parentNode.removeChild(img);
+			link.style.backgroundColor = "gray";
 		}
+		var classes = link.className;
+		link.className = classes+' pgloaded';
 	};
 	if(devmode == false) {
 		var timeout = setTimeout(function() { if(img) { img.parentNode.removeChild(img); } }, 30000);
@@ -21,39 +26,55 @@ function preload_gif(url) {
 }
 
 function remove_thumb(that) {
-that.parentNode.removeChild(that);
+	that.parentNode.removeChild(that);
 }
 
 function find_gifs() {
 	var links = document.getElementsByClassName('title');
-	var i = links.length;
+	//var i = links.length;
 	var href;
-	while ( i-- ) {
-		href = links[i].getAttribute('href');
-		if(href == null) {
+	for (var i in links) {
+		if(links[i].constructor == HTMLAnchorElement) {
+			href = links[i].getAttribute('href');
+		} else {
 			continue;
 		}
-		if ( links[i].tagName == 'A' && href != null && (
-			href.slice(-4) == '.gif'
-			|| href.search('.gif?') > 0)
-			/*|| (document.URL.search('gif') > -1 && href.search('imgur.com') >= 0 && href.search('imgur.com/a/') == -1) // if on a gif reddit try and preload imgur GIFs linked to page instead of GIF*/
-		) {
+		if(href == null) {
+			continue;
+		} else if(links[i].className.match(new RegExp('(\\s|^)'+'pgloaded'+'(\\s|$)'))) {
+			continue;
+		} else if ( links[i].tagName == 'A' && href != null && (
+				href.slice(-4) == '.gif'
+				|| href.search('.gif?') > 0)
+				/*|| (document.URL.search('gif') > -1 && href.search('imgur.com') >= 0 && href.search('imgur.com/a/') == -1) // if on a gif reddit try and preload imgur GIFs linked to page instead of GIF*/
+			) {
 			/*if(href.search('imgur.com') >= 0 && href.search('imgur.com/a/') == -1) {
 				href = href + '.gif';
 			}*/
 			if(ns == 1) {
 				if(links[i].parentNode.parentNode.childNodes[2].getElementsByClassName('nsfw-stamp').length == 0) {
-					preload_gif(href);
+					preload_gif(href, links[i]);
 				}
 			} else if(ns == 2) {
 				if(links[i].parentNode.parentNode.childNodes[2].getElementsByClassName('nsfw-stamp').length > 0) {
-					preload_gif(href);
+					preload_gif(href, links[i]);
 				}
 			} else {
-				preload_gif(href);
+				preload_gif(href, links[i]);
 			}
 		}
 	}
 }
 
+function checkNER() {
+	var ner = document.getElementsByClassName('NERPageMarker').length;
+	if(ner > NERCount) {
+		NERCount++;
+		find_gifs();
+	} else {
+		return false;
+	}
+}
+
 window.addEventListener('load', find_gifs());
+window.setInterval(checkNER, 500);
